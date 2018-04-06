17469ac301710feeea5342e6f121318dc801379a
Alex Contreras Barba <acontreras28@client149-10-192-46.eduroam.upc.edu>
2018-04-06 09:59:59 +0200

    Add tests

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './components/restore_credentials/restore_credentials';
 
 AppRegistry.registerComponent('Integrate', () => App);
