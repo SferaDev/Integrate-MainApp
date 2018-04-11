13fba590d4adaf15c5adf0492e8e3dffa1291056
Alex Contreras Barba <acontreras28@MacBook-Pro-de-Alex.local>
2018-04-11 14:29:11 +0200

    Add toast component

---
+++ components/restore_credentials/toast.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class Toast extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasBeenDisplayed: false
+        };
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        /*if (this.state.hasBeenDisplayed == false & this.props.visible) {
+        }*/
+    }
+
+    render() {
+        return (
+            <View style = {[styles.toast, {display: (this.props.visible) ? 'block' : 'none'}]}>
+                <Text>
+                    Toaast
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    toast: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'red',
+    }
+});
