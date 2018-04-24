be9bc21430cd15d6f8f5c7027365f271b68b647e
Aleix Sanfe <aleix.sanfeliu@gmail.com>
2018-04-24 09:55:32 +0200

    Set customMarker

--- components/buscador/marker_list.js
+++ components/buscador/marker_list.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {} from 'react-native';
 import MapView from 'react-native-maps';
+import MarkerImage from '../../Images/marker.png';
 
-const MarkerList = props =>  props.items.map((e, i) => { return(<MapView.Marker key={e.id} identifier={''+e.id+''} coordinate={ {latitude: e.addressLatitude, longitude: e.addressLongitude, latitudeDelta: 0.01,longitudeDelta: 0.01} } pinColor="red" />)} );
+const MarkerList = props =>  props.items.map((e, i) => { return(<MapView.Marker key={e.id} identifier={''+e.id+''} image={MarkerImage} coordinate={ {latitude: e.addressLatitude, longitude: e.addressLongitude, latitudeDelta: 0.01,longitudeDelta: 0.01} } pinColor="red" />)} );
 
 export default MarkerList;
