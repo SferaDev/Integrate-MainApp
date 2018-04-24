6968e27ce65bd7c88423e0ed0f259b13babb606c
Aleix Sanfe <aleix.sanfeliu@gmail.com>
2018-04-24 09:28:44 +0200

    Block backGesture for ios

--- components/home.js
+++ components/home.js
@@ -8,42 +8,51 @@ import App from '../App';
 const BuscadorStack = StackNavigator({
     buscador: {screen: Buscador},
 }, {
-    headerMode: 'none'
+    headerMode: 'none',
+    disabledBackGesture: true
 });
 
 const ValsStack = StackNavigator({
     app: {screen: App},
 }, {
-    headerMode: 'none'
+    headerMode: 'none',
+    disabledBackGesture: true
 });
 
 const ProfileStack = StackNavigator({
     app: {screen: App},
 }, {
-    headerMode: 'none'
+    headerMode: 'none',
+    disabledBackGesture: true
 });
 
 const ConfigStack = StackNavigator({
     app: {screen: App},
 }, {
-    headerMode: 'none'
+    headerMode: 'none',
+    disabledBackGesture: true
 });
 
 const DrawerStack = DrawerNavigator({
     Buscador: {screen: BuscadorStack}
+}, {
+    headerMode: 'none',
+    disabledBackGesture: true
 });
 
 
 const DrawerNavigation = StackNavigator({
     DrawerStack: {screen: DrawerStack}
 }, {
-    headerMode: 'none'
+    headerMode: 'none',
+    disabledBackGesture: true
 });
 
 const LoginStack = StackNavigator({
     Login: {screen: Login}
 }, {
     headerMode: 'none',
+    disabledBackGesture: true
 });
 
 // Manifest of possible screens
@@ -53,7 +62,8 @@ const Home = StackNavigator({
 }, {
     // Default config for all screens
     headerMode: 'none',
-    initialRouteName: 'LoginStack'
+    initialRouteName: 'LoginStack',
+    disabledBackGesture: true
 });
 
 export default Home;
