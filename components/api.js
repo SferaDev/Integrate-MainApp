e1740f3740d6957357933da3d7ad717477666e0d
Alexis Rico <SferaDev@users.noreply.github.com>
2018-04-30 18:35:30 +0200

    Update api.js

--- components/api.js
+++ components/api.js
@@ -8,58 +8,45 @@ const API = {
         return new Promise((resolve, reject) => {
             if (nifnie.length === 0 || password.length === 0) reject();
             else {
-
                 let url = 'login';
                 let params = [ {key: 'nif', value: nifnie} , {key: 'password', value: password} ];
-
                 let success = (response) => {
-
-                    if( response.status === 401 ){
+                    if (response.status === 401) {
                         reject();
-                    }else if( response.status === 200 ){
+                    } else if (response.status === 200){
                         AsyncStorage.setItem('token', JSON.parse(response._bodyText).token)
                         resolve(JSON.parse(response._bodyText).token);
                     }
                 }
-
                 httpHelper.callApi(url,params,success,reject);
             }
         });
     },
     getEntities: () => {
-
         return new Promise((resolve, reject) => {
-
             AsyncStorage.getItem('token').then( (token) => {
-
-                if(token){
-
+                if (token) {
                     let url = 'me/entities';
                     let params = [ {key: 'token', value: token} ];
-
                     let success = (response) => {
-                        if( response.status === 404 ){
+                        if (response.status === 404) {
                             reject();
-                        }else if( response.status === 200 ){
-                            resolve( JSON.parse(response._bodyText) );
+                        } else if (response.status === 200) {
+                            resolve(JSON.parse(response._bodyText));
                         }
                     }
-
                     httpHelper.callApi(url,params,success,reject);
-                }else{
+                } else {
                     reject();
                 }
-
-            } ).catch( () => {
-            } );
+            }).catch(() => {
+            });
         });
     },
     getGoods: (category = 0, order = 0, loc = null) => {
         return new Promise(function (resolve, reject) {
-
             AsyncStorage.getItem('token').then( (token) => {
-
-                if(token){
+                if (token) {
                     let url = BASEURL+'/me/goods?token='+token+'&category='+category+'&order='+order;
                     if (loc !== null) {
                         url += '&latitude='+loc.coords.latitude+'&longitude='+loc.coords.longitude;
@@ -76,7 +63,7 @@ const API = {
                             reject();
                         });*/
                     resolve([{name: 'good1'},{name: 'good2'}]);
-                }else{
+                } else {
                     reject();
                 }
 
