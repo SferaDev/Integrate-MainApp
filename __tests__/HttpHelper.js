0e64f0e98bae463517a62fbe27247b384902a1ec
Alexis Rico <SferaDev@users.noreply.github.com>
2018-04-30 18:30:01 +0200

    Update HttpHelper.js

--- __tests__/HttpHelper.js
+++ __tests__/HttpHelper.js
@@ -7,9 +7,7 @@ const fetchPolifill = require('whatwg-fetch')
 global.fetch = fetchPolifill.fetch
 
 describe('Test group for EntityList', function () {
-
     beforeAll(() => {
-
         configure({adapter: new Adapter()});
     });
 
@@ -18,30 +16,24 @@ describe('Test group for EntityList', function () {
     afterEach(function () {});
 
     describe("buildQuery() tests", () => {
-
 	    test('buildQuery() when url and params are blank', () => {
-
 		    expect(HttpHelper.buildQuery(undefined,undefined,'http://localhost:8081')).toBe("http://localhost:8081/");
 		});
 
 		test('buildQuery() when params are blank', () => {
-
 		    expect(HttpHelper.buildQuery('route1',undefined,'http://localhost:8081')).toBe("http://localhost:8081/route1");
 		});
 
 		test('buildQuery() when url and params are set', () => {
-
 		    expect(HttpHelper.buildQuery('route1',[{key: 'param1',value: 'value1'}],'http://localhost:8081')).toBe("http://localhost:8081/route1?param1=value1");
 		});
 
 		test('buildQuery() when url and params are set', () => {
-
 		    expect(HttpHelper.buildQuery('route1',[{key: 'param1',value: 'value1'},{key: 'param2',value: 'value2'}],'http://localhost:8081')).toBe("http://localhost:8081/route1?param1=value1&param2=value2");
 		});
 	});
 
 	test('callApi() is callable and returns nothing', () => {
-
 	    expect(HttpHelper.callApi()).toBe(undefined);
 	});
 
