152a991224247239be8bbf5199a9c0212ccb1756
acontreras28 <aleex.28.05@gmail.com>
2018-05-03 21:47:02 +0200

    Added styles to list goods

--- components/llista_vals/good.js
+++ components/llista_vals/good.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import API from "../api";
 
 export default class Good extends Component {
     constructor(props) {
@@ -7,24 +9,32 @@ export default class Good extends Component {
         this.state = {};
     }
 
+    addFavourites() {
+        console.warn("Afegir a fav")
+    }
+
     render() {
         return (
             <View key={this.props.item['_id']} style={styles.entityView}>
-                <Text>
-                    Cada {this.props.item.reusePeriod} dies
-                </Text>
-                <Text>
-                    -{this.props.item.discount} {this.props.item.discountType}
-                </Text>
-                <Text>
-                    {this.props.item.productName}
-                </Text>
+                <Text style={styles.goodBasic}>Cada {this.props.item.reusePeriod} dies</Text>
+                <Text style={styles.goodBasic}>Preu inicial: {this.props.item.initialPrice} €</Text>
+                <Text style={styles.goodBasic}>-{this.props.item.discount} {this.props.item.discountType}</Text>
+                <Text style={styles.entityName}>{this.props.item.productName}</Text>
+                <Text style={styles.goodSubText}>{this.props.item.category}</Text>
+
+                <Icon style={styles.favProps}
+                    name="star" size={25}
+                      onPress={this.addFavourites.bind(this)}/>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    favProps: {
+        flexDirection:'row',
+        paddingTop: 10,
+    },
     entityView: {
         borderColor: 'black',
         borderStyle: 'solid',
@@ -34,18 +44,22 @@ const styles = StyleSheet.create({
         paddingRight: 60,
     },
     entityName: {
-        fontSize: 24,
+        paddingTop: 5,
+        fontSize: 20,
         color: '#094671',
     },
-    entityDescription: {
-        fontSize: 14,
+    goodBasic: {
+        fontSize: 10,
+        paddingTop: 5,
         color: '#67acb1'
     },
-    entityAddress: {
-        fontSize: 14,
+    goodSubText: {
+        paddingTop: 5,
+        fontSize: 9,
         color: '#606060'
     },
     entityLikes: {
+        paddingTop: 3,
         position: 'absolute',
         right: 15,
         top: 15,
