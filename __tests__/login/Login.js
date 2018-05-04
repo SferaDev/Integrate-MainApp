0ee9d4015b1b47671b08f5adff0f3c9079102ea4
Alexis Rico <SferaDev@users.noreply.github.com>
2018-05-04 08:49:03 +0200

    Update Login.js

--- __tests__/login/Login.js
+++ __tests__/login/Login.js
@@ -109,30 +109,24 @@ describe('Test group for EntityList', function () {
     });
 
     describe("getButtonBackground() and getButtonColor() tests", () => {
-
         it('getButtonBackground() is callable and returns a color code', () => {
-            
             expect(typeof instance.getButtonBackground()).toBe("string");
         });
 
         it('getButtonBackground() is callable and returns a color code', () => {
-            
             instance.state.nifnie = '123456789';
             instance.state.password = 'QWERTY1234';
             expect(typeof instance.getButtonBackground()).toBe("string");
         });
 
         it('getButtonColor() is callable and returns a color code', () => {
-
             expect(typeof instance.getButtonColor()).toBe("string");
         });
 
         it('getButtonColor() is callable and returns a color code', () => {
-
             instance.state.nifnie = '123456789';
             instance.state.password = 'QWERTY1234';
             expect(typeof instance.getButtonColor()).toBe("string");
         });
-
     });
 });
