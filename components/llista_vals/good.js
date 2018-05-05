8066dfaf47b928779d295fd0f6f5da71721aa0a9
acontreras28 <aleex.28.05@gmail.com>
2018-05-05 03:42:35 +0200

    Good styles added and now we can save fav goods and they appear in the fav view

--- components/llista_vals/good.js
+++ components/llista_vals/good.js
@@ -12,32 +12,30 @@ export default class Good extends Component {
         };
     }
 
-    showError() {
-        console.warn("ERRORACO")
-    }
-
     addFavourites() {
         if (this.state.favourite == false) {
             this.setState({favourite: true});
-            API.addGoodFav(this.props.item['_id']).catch(this.showError.bind(this));
-            ;
+            API.addGoodFav(this.props.item['_id']);
         }
-        else this.setState({favourite: false});
-        console.warn("me/goods/favourites/" + this.props.item['_id'])
+        else {
+            this.setState({favourite: false});
+            API.deleteGoodFav(this.props.item['_id']);
+        }
     }
 
     render() {
         return (
             <View key={this.props.item['_id']} style={styles.entityView}>
-                <Text style={styles.goodBasic}>Cada {this.props.item.reusePeriod} dies</Text>
-                <Text style={styles.goodBasic}>Preu inicial: {this.props.item.initialPrice} €</Text>
-                <Text style={styles.goodBasic}>-{this.props.item.discount} {this.props.item.discountType}</Text>
-                <Text style={styles.entityName}>{this.props.item.productName}</Text>
-                <Text style={styles.goodSubText}>{this.props.item.category}</Text>
+                <Text style={styles.goodBasicText}>Cada {this.props.item.reusePeriod} dies</Text>
+                <Text style={styles.goodBasicText}>Preu inicial: {this.props.item.initialPrice} €</Text>
+                <Text style={styles.goodBasicText}>-{this.props.item.discount} {this.props.item.discountType}</Text>
+                <Text style={styles.entityName}>{this.props.item.owner.name}</Text>
+                <Text style={styles.goodBasicText}>{this.props.item.productName}</Text>
 
-                <Icon style={styles.favProps}
+                <Icon style={[styles.favProps, {color: (this.state.favourite) ? '#f4eb49' : '#CCC'}]}
                       name="star" size={25}
-                      onPress={this.addFavourites.bind(this)}/>
+                      onPress={this.addFavourites.bind(this)}
+                />
             </View>
         );
     }
@@ -47,36 +45,32 @@ const styles = StyleSheet.create({
     favProps: {
         flexDirection:'row',
         paddingTop: 10,
+        alignSelf: 'flex-end',
+        color: '#f4eb49',
     },
     entityView: {
         borderColor: 'black',
         borderStyle: 'solid',
+        backgroundColor: 'white',
         borderTopWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
         borderBottomWidth:1,
-        padding: 15,
-        paddingRight: 60,
+        alignSelf: 'center',
+        height: 166,
+        width: 325,
     },
     entityName: {
         paddingTop: 5,
-        fontSize: 20,
-        color: '#094671',
+        paddingLeft: 15,
+        fontSize: 18,
+        color: '#232323',
+        fontWeight: 'bold'
     },
-    goodBasic: {
-        fontSize: 10,
-        paddingTop: 5,
-        color: '#67acb1'
+    goodBasicText: {
+        fontSize: 14,
+        paddingTop: 8,
+        paddingLeft: 15,
+        color: '#232323'
     },
-    goodSubText: {
-        paddingTop: 5,
-        fontSize: 9,
-        color: '#606060'
-    },
-    entityLikes: {
-        paddingTop: 3,
-        position: 'absolute',
-        right: 15,
-        top: 15,
-        display: 'flex',
-        flexDirection: 'row',
-    }
 });
