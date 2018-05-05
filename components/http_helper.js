f52e508c5c3cef24bb5ba40c77f6de638230ecb1
Alexis Rico <SferaDev@users.noreply.github.com>
2018-05-05 08:28:29 +0200

    Update http_helper.js

--- components/http_helper.js
+++ components/http_helper.js
@@ -1,24 +1,24 @@
-const BASEURL = 'http://integrate-backend-staging.herokuapp.com';
+const BASE_URL = 'http://integrate-backend-staging.herokuapp.com';
 
-const buildQuery = (url = '',params = [],base_url = BASEURL) => {
+const buildQuery = (url = '', params = [], base_url = BASE_URL) => {
     let query = base_url + '/' + url;
     let keys = Object.keys(params);
 
     for (let i in keys) {
-        if(i == 0)query += '?';
+        if (i == 0) query += '?';
         else query += '&';
-        query += params[ keys[i] ].key + '=' + params[ keys[i] ].value;
+        query += params[keys[i]].key + '=' + params[keys[i]].value;
     }
     return query;
 }
 
-const callApi = (url,params,success,error,method="GET") => {
-    fetch(buildQuery(url,params),{method:method}).then(success).catch(error);
+const callApi = (url, params, success, error, method = 'GET') => {
+    fetch(buildQuery(url, params), { method: method }).then(success).catch(error);
 }
 
 const httpHelper = {
-	buildQuery: buildQuery,
-	callApi: callApi
+    buildQuery: buildQuery,
+    callApi: callApi
 }
 
 export default httpHelper;
