1b46452ceee6f9b27eab870c12cda682c77d79f2
Pol Moya Betriu <moyapol@gmail.com>
2018-05-24 17:34:07 +0200

    fix/icons: Add icons to project (#40)
    * Added IOS icons
    
    * Added Android icons
    
    * Added Android icons resized
    
    * Added Android icons resized
    
    * ReOrder Navigation Drawer and restyled login, toast & home buttons
    
    * ReStyled Conflict message

--- components/login/login.js
+++ components/login/login.js
@@ -135,6 +135,7 @@ export default class LogIn extends Component {
                         <TouchableHighlight
                             style={[styles.button, {backgroundColor: this.getButtonBackground()}]}
                             onPress={this.login.bind(this)}
+                            underlayColor='none'
                             disabled={this.isEmpty()}>
                             <Text style={{alignSelf: 'center', color: this.getButtonColor(), fontWeight: 'bold'}}>
                                 Entra
