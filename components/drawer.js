6624cb12ad313d0f167d2edda1e4097d4ee01db1
Aleix Sanfe <aleix.sanfeliu@gmail.com>
2018-06-01 20:39:25 +0200

    Set whole drawer item clickable

--- components/drawer.js
+++ components/drawer.js
@@ -1,49 +1,50 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {NavigationActions} from 'react-navigation';
-import {ScrollView, Text, View, StyleSheet, Image} from 'react-native';
+import {ScrollView, Text, View, StyleSheet, Image, TouchableHighlight} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import language_settings from './language_settings';
 
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    flex: 1
-  },
-  navSectionStyle: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignContent: 'center',
-    borderColor: 'lightgrey',
-    borderBottomWidth: 0.5,
-    borderTopWidth: 0.5,
-    paddingTop: 5,
-    paddingBottom: 5
-  },
-  navItemStyle: {
-    padding: 10,
-    fontSize: 18,
-    alignSelf: 'center'
-  },
-  navItemLogo:{
-    alignSelf: 'center',
-    marginLeft: 15,
-    marginRight: 15
-  },
-  footerContainer: {
-    padding: 20,
-    backgroundColor: 'lightgrey'
-  }
+    container: {
+        paddingTop: 20,
+        flex: 1
+    },
+    navSection: {
+        borderColor: 'lightgrey',
+        borderBottomWidth: 0.5,
+        borderTopWidth: 0.5,
+        paddingTop: 5,
+        paddingBottom: 5
+    },
+    navItem: {
+        display: 'flex',
+        flexDirection: 'row', 
+        alignContent: 'center'
+    },
+    navItemLabel: {
+        padding: 10,
+        fontSize: 18,
+        alignSelf: 'center'
+    },
+    navItemLogo:{
+        alignSelf: 'center',
+        marginLeft: 15,
+        marginRight: 15
+    },
+        footerContainer: {
+        padding: 20,
+        backgroundColor: 'lightgrey'
+    }
 });
 
 class Drawer extends Component {
-  navigateToScreen = (route) => () => {
-    const navigateAction = NavigationActions.navigate({
-      routeName: route
-    });
-    this.props.navigation.dispatch(navigateAction);
-  }
+    
+    navigateToScreen(route){
+        const navigateAction = NavigationActions.navigate({routeName: route});
+        this.props.navigation.dispatch(navigateAction);
+    }
 
     render () {
         return (
@@ -51,26 +52,35 @@ class Drawer extends Component {
                 <Image source={require('../Images/ic_launcher.png')} style={{height: 100, width: 100,alignSelf: 'center',marginBottom: 20}} />
                 <ScrollView>
 
-                <View style={styles.navSectionStyle}>
-                    <Icon style={styles.navItemLogo} name="home" size={25}/>
-                    <Text style={styles.navItemStyle} onPress={this.navigateToScreen('Buscador')}>{language_settings[ global.lang ].home.searcher}</Text>
-                </View>
+                    <TouchableHighlight style={styles.navSection} onPress={() => this.navigateToScreen('Buscador')} underlayColor="white">
+                        <View style={styles.navItem} >
+                            <Icon style={styles.navItemLogo} name="home" size={25}/>
+                            <Text style={styles.navItemLabel}>{language_settings[ global.lang ].home.searcher}</Text>
+                        </View>
+                    </TouchableHighlight>
 
-                <View style={styles.navSectionStyle}>
-                    <Icon style={styles.navItemLogo} name="ticket-percent" size={25}/>
-                    <Text style={styles.navItemStyle} onPress={this.navigateToScreen('Vals')}>{language_settings[ global.lang ].home.goods}</Text>
-                </View>
+                    <TouchableHighlight style={styles.navSection} onPress={() => this.navigateToScreen('Vals')} underlayColor="white">
+                        <View style={styles.navItem} >
+                            <Icon style={styles.navItemLogo} name="ticket-percent" size={25}/>
+                            <Text style={styles.navItemLabel}>{language_settings[ global.lang ].home.goods}</Text>
+                        </View>
+                    </TouchableHighlight>
 
-                <View style={styles.navSectionStyle}>
-                    <Icon style={styles.navItemLogo} name="settings" size={25}/>
-                    <Text style={styles.navItemStyle} onPress={this.navigateToScreen('Profile')}>{language_settings[ global.lang ].home.settings}</Text>
-                </View>
+                    <TouchableHighlight style={styles.navSection} onPress={() => this.navigateToScreen('Profile')} underlayColor="white">
+                        <View style={styles.navItem} >
+                            <Icon style={styles.navItemLogo} name="settings" size={25}/>
+                            <Text style={styles.navItemLabel}>{language_settings[ global.lang ].home.settings}</Text>
+                        </View>
+                    </TouchableHighlight>
 
                 </ScrollView>
-                <View style={[styles.navSectionStyle,{height: 75}]}>
-                    <Icon style={styles.navItemLogo} name="logout-variant" size={25}/>
-                    <Text style={styles.navItemStyle} onPress={this.navigateToScreen('Logout')}>{language_settings[ global.lang ].home.log_out}</Text>
-                </View>
+                
+                <TouchableHighlight style={styles.navSection} onPress={() => this.navigateToScreen('Logout')} underlayColor="white">
+                    <View style={[styles.navItem,{height: 75}]} >
+                        <Icon style={styles.navItemLogo} name="logout-variant" size={25}/>
+                        <Text style={styles.navItemLabel}>{language_settings[ global.lang ].home.log_out}</Text>
+                    </View>
+                </TouchableHighlight>
             </View>
         );
     }
