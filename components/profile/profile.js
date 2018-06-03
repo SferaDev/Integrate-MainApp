e25d4ee9a8171b28c11d422c4bc3e1ecb47ffb66
Aleix Sanfe <aleix.sanfeliu@gmail.com>
2018-06-03 16:01:58 +0200

    Add about information

--- components/profile/profile.js
+++ components/profile/profile.js
@@ -70,8 +70,15 @@ export default class Profile extends Component {
 
                         <ChangePassword style={{width: this.state.Dwidth}} />
 
-                        <View style={{width: this.state.Dwidth}} >
-                            <Text>HELLO WORLD</Text>
+                        <View style={[styles.container,{width: this.state.Dwidth}]}>
+                            
+                            <Text style={{fontSize: 32}}>
+                                Integrate SL
+                            </Text>
+                            <Text style={{width: 275,fontSize: 16,textAlign: 'center',paddingTop: 10,paddingBottom: 10}}>
+                                {language_settings[global.lang].about.description}
+                            </Text>
+                            
                         </View>
 
                         <View style={{width: this.state.Dwidth}} >
