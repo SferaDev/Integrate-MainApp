90104aa2e88c76f9d70a36b64339e69587732098
Aleix Sanfe <aleix.sanfeliu@gmail.com>
2018-06-10 22:04:37 +0200

    Fix DetallsGoods tests

--- __tests__/llista_vals/DetallsGood.js
+++ __tests__/llista_vals/DetallsGood.js
@@ -7,27 +7,36 @@ import DetallsGood from '../../components/llista_vals/detalls_good';
 
 jest.mock('../../components/http_helper');
 
-const navigation = {navigate: jest.fn()};
-const good = {
-    _id: 1,
-    productName: 'name',
-    initialPrice: 24,
-    category: 2,
-    owner: {
-        name: 'NAME'
-    }
+const navigation = {
+    navigate: jest.fn(),
+    state: {
+        params: {
+            selectedGood: {
+                _id: 1,
+                productName: 'name',
+                initialPrice: 24,
+                category: 2,
+                owner: {
+                    name: 'NAME'
+                }
+            },
+            isFav: true,
+            isEntityDisplay: true,
+            toggleFavourite: jest.fn()
+        }
+    },
 };
 
-describe('Test group for EntityList', function () {
+describe('Test group for DetallsGood', function () {
+
     beforeAll(() => {
         configure({adapter: new Adapter()});
         global.lang = 'ca';
     });
 
     beforeEach(function () {
-        // Before each: Shallows the EntityList component
-        wrapper = shallow(<DetallsGood navigation={navigation} good={good} isFav={true} showGoodsList={jest.fn()}
-                                       toggleFavourite={jest.fn()} context={this}/>);
+
+        wrapper = shallow(<DetallsGood navigation={navigation} />);
         instance = wrapper.instance();
     });
 
@@ -38,21 +47,41 @@ describe('Test group for EntityList', function () {
     });
 
     test('renders detalls_entitat correctly', () => {
-        let component = renderer.create(<DetallsGood good={good} isFav={true} showGoodsList={jest.fn()}
-                                                     toggleFavourite={jest.fn()} context={this}/>);
+        let component = renderer.create(<DetallsGood navigation={navigation} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     test('renders detalls_entitat correctly', () => {
-        let component = renderer.create(<DetallsGood good={good} isFav={false} showGoodsList={jest.fn()}
-                                                     toggleFavourite={jest.fn()} context={this}/>);
+        let component = renderer.create(<DetallsGood navigation={navigation} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     test('showEntity is callable and returns nothing', () => {
         expect(instance.showEntity()).toBe(undefined);
+    });
+
+    test('goBack is callable and returns nothing', () => {
+        expect(instance.goBack()).toBe(undefined);
+    });
+
+    test('toggleFavourite is callable and returns nothing',await () => {
+
+        navigation.state.params.isFav = false;
+        wrapper = shallow(<DetallsGood navigation={navigation} />);
+        instance = wrapper.instance();
+
+        expect(async instance.toggleFavourite()).toBe(true);
+    });
+
+    test('toggleFavourite is callable and returns nothing',await () => {
+
+        navigation.state.params.isFav = true;
+        wrapper = shallow(<DetallsGood navigation={navigation} />);
+        instance = wrapper.instance();
+
+        expect(async instance.toggleFavourite()).toBe(false);
     });
 
 });
