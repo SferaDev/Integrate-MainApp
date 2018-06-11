322c838c4048e9c618821cd97e0dbccc4c9f3b36
Pol Moya Betriu <pol.moya.betriu@est.fib.upc.edu>
2018-06-11 19:43:06 +0200

    Added tests Good

--- __tests__/compra/Good.js
+++ __tests__/compra/Good.js
@@ -2,11 +2,21 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+jest.mock('../../components/http_helper');
+
 import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Good from '../../components/compra/good';
 
-const navigation = {navigate: jest.fn()};
+const navigation = {
+    navigate: jest.fn(),
+    state: {
+        params: {
+
+        }
+    },
+    goBack: jest.fn()
+};
 let wrapper;
 let instance;
 let good;
@@ -21,22 +31,26 @@ describe('Test group for good', function () {
             category: 2,
             owner: {
                 name: 'NAME'
-            }
+            },
+            reusePeriod: 1
         };
         global.lang = 'ca';
     });
 
     beforeEach(function () {
         // Before each: Shallows the EntityList component
-        wrapper = shallow(<Good
-                                type={0}
-                                item={good}
-                                context={this}
-                                isEntityDisplay={true}
-                                isFav={true}
-                                toggleFavourite={jest.fn()}
-                                navigation={jest.fn()}  //No ho tinc clar
-        />);
+        wrapper = shallow(
+            <Good
+                type={0}
+                item={good}
+                context={this}
+                isEntityDisplay={true}
+                isFav={true}
+                isSelected={false}
+                toggleFavourite={jest.fn()}
+                navigation={navigation}
+            />
+        );
         instance = wrapper.instance();
     });
 
@@ -50,34 +64,54 @@ describe('Test group for good', function () {
         expect(instance.selectGood()).toBe(undefined);
     });
 
-    /*test('getPeriodText is callable and returns nothing', () => {
-        expect(instance.getPeriodText()).toBe(undefined);
+    test('getPeriodText is callable and returns nothing', () => {
+        expect(typeof instance.getPeriodText()).toBe("string");
     });
 
     test('getDiscountText is callable and returns nothing', () => {
-        expect(instance.getDiscountText()).toBe(undefined);
-    });*/
+        expect(typeof instance.getDiscountText()).toBe("string");
+    });
 
     test('showGoodDetails is callable and returns nothing', () => {
         expect(instance.showGoodDetails()).toBe(undefined);
     });
 
-    test('toggleFavourite is callable and returns nothing', () => {
-        expect(instance.toggleFavourite()).toBe(undefined);
+    test('toggleFavourite is callable and returns nothing',async () => {
+        expect(await instance.toggleFavourite()).toBe(false);
     });
 
-    /*describe("renderGood() tests", () => {
+    describe("renderGood() tests", () => {
 
         test('renderGood when type = 1 ', () => {
-            expect(instance.renderGood(1)).toBe(undefined);
+            expect(instance.renderGood(1)).toMatchSnapshot();
         });
 
         test('renderGood when type = 2 ', () => {
-            expect(instance.renderGood(2)).toBe(undefined);
+            expect(instance.renderGood(2)).toMatchSnapshot();
         });
 
         test('renderGood when type is != 1 and != 2 ', () => {
-            expect(instance.renderGood(3)).toBe(undefined);
+            expect(instance.renderGood(0)).toMatchSnapshot();
         });
-    });*/
+    });
+
+    describe("renderBuyGood() tests", () => {
+
+        test('renderBuyGood when isUsable = false ', () => {
+            instance.props.item.isUsable = true;
+            expect(instance.renderBuyGood()).toMatchSnapshot();
+        });
+
+        test('renderBuyGood when isUsable = true and isSelected = true ', () => {
+            instance.props.item.isUsable = true;
+            instance.props.isSelected = true;
+            expect(instance.renderBuyGood()).toMatchSnapshot();
+        });
+
+        test('renderBuyGood when isUsable = true and isSelected = false', () => {
+            instance.props.item.isUsable = true;
+            instance.props.isSelected = false;
+            expect(instance.renderBuyGood()).toMatchSnapshot();
+        });
+    });
 });
