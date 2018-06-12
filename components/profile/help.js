668debf627b8e1db6db09b0500489897ea89288c
acontreras28 <aleex.28.05@gmail.com>
2018-06-12 03:03:13 +0200

    Help view refactor

--- components/profile/help.js
+++ components/profile/help.js
@@ -15,40 +15,39 @@ export default class Help extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
+            <View>
                 <View style={styles.header}>
                     <Icon onPress={this.openMenu.bind(this)} style={styles.headerLeftIco} name="menu" size={30}/>
                 </View>
-
-                <View style={styles.body}>
-                    <Text style={[styles.text, {fontWeight: 'bold'}]}>
-                        {language_settings[global.lang].help.title1}
-                    </Text>
-                    <Text style={styles.text}>
-                        {language_settings[global.lang].help.text1}
-                    </Text>
-
-                    <Text style={[styles.text, {fontWeight: 'bold'}]}>
-                        {language_settings[global.lang].help.title2}
-                    </Text>
-                    <Text style={styles.text}>
-                        {language_settings[global.lang].help.text2}
-                    </Text>
-
-                    <Text style={[styles.text, {fontWeight: 'bold'}]}>
-                        {language_settings[global.lang].help.title3}
-                    </Text>
-                    <Text style={styles.text}>
-                        {language_settings[global.lang].help.text3}
-                    </Text>
-
-                    <Text style={[styles.text, {fontWeight: 'bold'}]}>
-                        {language_settings[global.lang].help.title4}
-                    </Text>
-                    <Text style={styles.text}>
-                        {language_settings[global.lang].help.text4}
-                    </Text>
-                </View>
+                <ScrollView style={{backgroundColor: '#F4F3F2'}}>
+                    <View style={styles.container}>
+                        <View style={styles.body}>
+                            <Text style={styles.category}>{language_settings[global.lang].help.category1}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question1}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer1}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question2}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer2}</Text>
+                            <Text style={styles.category}>{language_settings[global.lang].help.category2}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question3}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer3}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question4}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer4}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question5}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer5}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question6}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer6}</Text>
+                            <Text style={styles.category}>{language_settings[global.lang].help.category3}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question7}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer7}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question8}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer8}</Text>
+                            <Text style={styles.question}>{language_settings[global.lang].help.question9}</Text>
+                            <Text style={styles.answer}>{language_settings[global.lang].help.answer9}</Text>
+                            <Text style={styles.answer}> </Text>
+                            <Text style={styles.answer}> </Text>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -73,28 +72,52 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         paddingLeft: 20,
         textAlign: 'left',
-        color: 'white'
+        color: 'white',
     },
     headerRightIco: {
         flex: 1,
         alignSelf: 'center',
         paddingRight: 20,
         textAlign: 'right',
-        color: 'white'
+        color: 'white',
     },
     body: {
         flex: 8,
         alignItems: 'center',
-        justifyContent: 'center',
+        //justifyContent: 'center',
         backgroundColor: '#F4F3F2',
         width: '100%',
-        height: '100%'
+        height: '100%',
     },
-    text:  {
-        width: 275,
+    category:  {
+        width: '100%',
+        fontSize: 22,
+        textAlign: 'justify',
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+        paddingTop: 25,
+        paddingBottom: 8,
+        paddingLeft: 35,
+        paddingRight: 35,
+    },
+    question:  {
+        width: '100%',
+        fontSize: 16,
+        textAlign: 'justify',
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        paddingTop: 8,
+        paddingBottom: 8,
+        paddingLeft: 35,
+        paddingRight: 35,
+    },
+    answer:  {
+        width: '100%',
         fontSize: 16,
         textAlign: 'justify',
         paddingTop: 8,
-        paddingBottom: 8
+        paddingBottom: 8,
+        paddingLeft: 50,
+        paddingRight: 50,
     },
 });
